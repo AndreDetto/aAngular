98ca3bc7f7dd55e7e4d2eeaf794bcfbd6d513165
Andre <dettopavan@gmail.com>
2022-05-05 18:24:27 +0200

    not even close

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/Components/navbar/navbar.component';
+import { IntroComponent } from './Components/Components/intro/intro.component';
+import { CoursesComponent } from './Components/Components/courses/courses.component';
+import { FooterComponent } from './Components/Components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IntroComponent,
+    CoursesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
